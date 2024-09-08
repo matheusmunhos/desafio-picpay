0d1f41954eddda9e12ebf7273cb672b03329f375
Matheus Munhos <matheus.munhos2005@gmail.com>
2024-09-07 23:46:02 -0300

    fix: name feign

--- src/main/java/br/com/munhosdev/picpay/client/AuthorizationClient.java
+++ src/main/java/br/com/munhosdev/picpay/client/AuthorizationClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(url = "${client.authorization.service.url}")
+@FeignClient(name = "authorizationClient", url = "${client.authorization.service.url}")
 public interface AuthorizationClient {
 
     @GetMapping
